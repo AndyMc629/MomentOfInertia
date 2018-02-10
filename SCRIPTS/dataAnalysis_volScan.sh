c91425aa3259396393561628ea8beb7e093f6365
APM13 <andrewpmcmahon629@gmail.com>
2018-02-10 12:04:03 +0000

    correct data analysis vol scan

--- SCRIPTS/dataAnalysis_volScan.sh
+++ SCRIPTS/dataAnalysis_volScan.sh
@@ -2,13 +2,12 @@
 
 home=$PWD
 today=`date '+%d_%m_%Y'`;
-for i in 0.925 0.95 0.975 1.00 1.025 1.05 1.075; do 
+for i in 0.90 0.95 1.00 1.05 1.10; do 
 	# pad out the numbers 
 	E=$(tail -1 Vol-${i}/OSZICAR)
-	V=$(grep -m 1 'volume of cell' Vol-${i}/OUTCAR | grep -o '[0-9]*\.[0-9]*')
-	echo "${V} ${E}" > E_${i} 	
+	echo "${i} ${E}" > E_${i} 	
 	P=$(grep 'pressure' Vol-${i}/OUTCAR)
-	echo "${V} ${P}" > P_${i}
+	echo "${i} ${P}" > P_${i}
 	cd $home
 done
 cat E_* > Energy.dat
